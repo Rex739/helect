a084da2c81780ea09983df23093b3d6188558ca1
user <ojemehprogress@gmail.com>
2020-07-14 14:04:12 +0100

    aa

--- index.php
+++ index.php
@@ -15,12 +15,41 @@ if(isset($_POST['submit'])){
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<meta charset="utf-8">
+	<!-- WEBSITE DESCRIPTION -->
+	<meta name="description" content="PETE: MUSIC TEMPLATE">
+	<meta name="keyword" content="HTML, CSS, BOOTSTRAP, JAVACRIPT">
+	<meta name="author" centent="Rex[02]">
+	<!-- OPTIMIZIED VIEWPORT FOR MOBILE -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale =1.0">
+	<title>Helect</title>
+	<!-- GOOGLE FONTS -->
+	<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@300&display=swap" rel="stylesheet">
+	<!-- FAVICON -->
+	<link rel="icon" type="image/png" href="images/favicon.png" sizes="32X32">
+
+	<!-- GLOBAL CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+	<!-- ADD YOUR CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
 </head>
 <body>
 	<form method="post">
 		<input type="email" name="email" placeholder="Enter Your Email">
 		<input type="submit" name="submit">
 	</form>
+
+	<script type="text/javascript" src="assets/js/jquery-3.5.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/popper.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/wow.js"></script>
+	<script>
+	   new WOW().init();
+    </script>
 </body>
 </html>
